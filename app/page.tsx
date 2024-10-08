560eff8d31baa39bf005ffa5d97dbd72302f5aab
sanjay2738873827348 <78831453+sanjay2738873827348@users.noreply.github.com>
2024-10-07 21:38:43 -0400

    revert back to 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,12 @@
 import Hero from "@/components/hero";
 import Navbartop from "@/components/navbar";
 import TdModule from "@/components/TdModule";
-import { NavigationMenuDemo } from "@/components/navbar2";
 
 export default function Home() {
   return (
     <main className="">
-    <div className="relative z-10">
-      <NavigationMenuDemo />  
-    </div>
-    <div className="relative z-0">
+      <Navbartop />
       <Hero />
-    </div>
-
-
-    {/* These are some placeholder break tags to simulate content for testing out scroll animations*/}
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>    
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
     </main>
   );
 }
